9abf225ac8e857ca26cb7a06cf77296bbe3a35bb
Juliana <julianareina.c@hotmail.com>
2023-08-21 21:47:59 -0500

    first commit

---
+++ src/main/java/com/uniquindio/AgileSoftwareDevelopment/SouthCoastNauticalSuppliesApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.AgileSoftwareDevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouthCoastNauticalSuppliesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouthCoastNauticalSuppliesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniquindio/AgileSoftwareDevelopment/SouthCoastNauticalSuppliesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.AgileSoftwareDevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouthCoastNauticalSuppliesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
